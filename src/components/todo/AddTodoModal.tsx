384b2201d1a73c7ce541965237a42ed88a8bb66b
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-28 13:05:09 +0600

    Shadcn components use

---
+++ src/components/todo/AddTodoModal.tsx
@@ -0,0 +1,44 @@
+import { Button } from "../ui/button"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+
+
+const AddTodoModal = () => {
+    return (
+        <Dialog>
+            <form>
+                <DialogTrigger asChild>
+                    <Button variant="outline">Add Task</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re
+                            done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="name-1">Name</Label>
+                            <Input id="name-1" name="name" defaultValue="Task1" />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="username-1">Decription</Label>
+                            <Input id="username-1" name="username" defaultValue="@peduarte" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </form>
+        </Dialog>
+    )
+}
+
+export default AddTodoModal
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,22 +1,26 @@
 import { Button } from "../ui/button"
+import AddTodoModal from "./AddTodoModal"
 import Todocard from "./Todocard"
 
 
 const TodoContainer = () => {
     return (
         <div>
-            <div>
-                <Button>Add Todo</Button>
-                <Button>Filter</Button>
+            <div className="flex justify-between mb-5">
+                <Button className="bg-primary-gradient text-xl font-semibold">Add Todo</Button>
+                <AddTodoModal />
+                <Button className="text-xl font-semibold">Filter</Button>
             </div>
-            <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
+            <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]" >
                 {/* <div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl font-bold">
                     <p >There is no task pending</p>
                 </div> */}
-                <Todocard />
-                <Todocard />
-                <Todocard />
-                <Todocard />
+                <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+                    <Todocard />
+                    <Todocard />
+                    <Todocard />
+                    <Todocard />
+                </div>
             </div>
         </div>
     )
