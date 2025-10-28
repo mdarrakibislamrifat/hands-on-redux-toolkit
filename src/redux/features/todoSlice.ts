d58506d9b1a6abb0739620628ea0e329640d3632
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-28 11:57:12 +0600

    Redux setup

---
+++ src/redux/features/todoSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+
+const initialState = {
+    todos: []
+}
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        
+    }
+})
+
+
+export const {}=todoSlice.actions
+
+export default todoSlice.reducer
